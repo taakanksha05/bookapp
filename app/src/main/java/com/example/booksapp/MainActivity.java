5cb3f15f2b720c601346241d8d06c2a1eb1b9d25
raje0616 <62427571+raje0616@users.noreply.github.com>
2022-03-24 22:04:48 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/booksapp/MainActivity.java
+++ app/src/main/java/com/example/booksapp/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.booksapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> book_id, book_title, book_author, book_pages;
+    CustomerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +30,36 @@ public class MainActivity extends AppCompatActivity {
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                Intent intent = new Intent(MainActivity.this,
+                        AddActivity.class);
                 startActivity(intent);
             }
         });
+
+        myDB = new MyDatabaseHelper(MainActivity.this);
+        book_id = new ArrayList<>();
+        book_title = new ArrayList<>();
+        book_author = new ArrayList<>();
+        book_pages = new ArrayList<>();
+
+        storeDataInArrays();
+        customerAdapter = new CustomerAdapter(MainActivity.this,book_id,book_title,book_author,book_pages);
+        recyclerView.setAdapter(customerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    void storeDataInArrays()
+    {
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0)
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
+        else        {
+            while (cursor.moveToNext()){
+                book_id.add(cursor.getString(0));
+                book_title.add(cursor.getString(1));
+                book_author.add(cursor.getString(2));
+                book_pages.add(cursor.getString(3));
+            }
+        }
     }
 }
